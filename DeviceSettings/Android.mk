44dfb1410f623ffa060421f81e1ea3bb00ea0a3f
Andrew Thomas <itz63c@statixos.com>
2020-06-04 22:59:55 -0400

    Revert "sdm845-common: DeviceSettings: Add Touchscreen Gesture controls"
    This reverts commit a1de8141735885e8eb0bfa7fd94fdec296420248.

--- DeviceSettings/Android.mk
+++ DeviceSettings/Android.mk
@@ -30,7 +30,6 @@ LOCAL_USE_AAPT2 := true
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx.core_core \
     androidx.preference_preference \
-    VendorSupportLib
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
