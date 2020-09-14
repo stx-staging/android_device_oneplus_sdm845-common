2b41be5cf9a5da42bf3d2e22192f3247fdd35771
Andrew Thomas <itz63c@statixos.com>
2020-09-14 18:19:38 -0400

    sdm845-common: [TEMP] go awae DeviceSettings

--- common.mk
+++ common.mk
@@ -62,8 +62,8 @@ PRODUCT_PACKAGES += \
     AntHalService
 
 # AOSiP Device Settings
-PRODUCT_PACKAGES += \
-    DeviceSettings
+# PRODUCT_PACKAGES += \
+#    DeviceSettings
 
 # Audio
 PRODUCT_PACKAGES += \
